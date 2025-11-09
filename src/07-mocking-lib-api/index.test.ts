cfa71b08a25c8c545f8b1579999ad6acd9ca11d2
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-09 23:00:02 +0100

    feat: add 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,61 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+jest.useFakeTimers();
 
-describe('throttledGetDataFromApi', () => {
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+
+const response = {
+  data: [],
+};
+
+describe('throttledGetDataFromApi (Оптимальная реализация)', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (axios.create as jest.Mock).mockReturnValue({ data: response });
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: response }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    await throttledGetDataFromApi('/posts');
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  test('should perform request to correct provided url after throttling delay', async () => {
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: response }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    await throttledGetDataFromApi('/posts');
+    await jest.advanceTimersByTimeAsync(6000);
+
+    expect(axiosClient.get).toHaveBeenCalledWith('/posts');
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test('should return correct response data', async () => {
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: response }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    const result = await throttledGetDataFromApi('/posts');
+
+    expect(result).toEqual(response);
   });
 });
