ebfae17e0980ac3355bb8885bb4790a62a9ad02b
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-09 18:42:11 +0100

    feat: add 01-simple-test implementation

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,88 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: Action.Add,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBe(2);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: Action.Subtract,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBe(0);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: Action.Multiply,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBe(1);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: Action.Divide,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBe(1);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: Action.Exponentiate,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBe(1);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: 1,
+      action: '111',
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const input = {
+      a: 1,
+      b: '111',
+      action: Action.Add,
+    };
+
+    const result = simpleCalculator(input);
+
+    expect(result).toBeNull();
   });
 });
